c64f652c480b6b487941dea7855f5182dac1339e
Geleta <geleta@Geletas-MBP.attlocal.net>
2024-05-28 23:55:33 -0400

    view en

--- src/app/my-custom-element/my-custom-element.component.ts
+++ src/app/my-custom-element/my-custom-element.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-my-custom-element',
   templateUrl: './my-custom-element.component.html',
-  styleUrls: ['./my-custom-element.component.css']
+  styleUrls: ['./my-custom-element.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class MyCustomElementComponent {
 
